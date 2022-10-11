ae0be2a46e786b2e3a3155ac1d86bce88ea7d6ec
student <We>
2022-10-11 12:32:43 -0400

    asdf

--- src/stringRunner.java
+++ src/stringRunner.java
@@ -5,9 +5,12 @@ public class stringRunner {
         int strLen = varName.length();
         System.out.println(varName.indexOf("you"));
 
-        String varName1 = "Hello how are you today?";
+        String varName1 = "AP CSA is awesome!";
         String aweStr = varName1.substring(10,13);
         System.out.println(aweStr);
         System.out.println(varName1);
+
+        String awe = varName1.substring(10,17);
+        System.out.println(awe);
     }
 }
